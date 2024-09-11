e9a500744f6839b1876d07a49d0b7b361c58b2ac
Aparna <iamaparnasurendran@gmail.com>
2024-09-11 12:20:35 +0530

    commit

--- Tivona/settings.py
+++ Tivona/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fmmc!%sw1fzh*@ospjdg&t327$*!$)*&@)(wzqb0u)vc+q!5mn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.110.108.8','0.0.0.0','localhost']
+ALLOWED_HOSTS = ['3.110.108.8','0.0.0.0']
 CSRF_TRUSTED_ORIGINS = [
 	'http://3.110.108.8',
 	'https://3.110.108.8',
